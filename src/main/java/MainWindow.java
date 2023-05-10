3dd01cc5ac9980dbea31c64b5b0ee8abcbcce439
FlamingSquid01 <brandonw5075@newulm.k12.mn.us>
2023-05-10 18:51:09 +0000

    B

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,4 +1,3 @@
-import java.util.concurrent.TimeUnit;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -15,18 +14,23 @@ public class MainWindow extends Canvas
   public static int cursorX;
   public static int cursorY;
 
-	private MouseAdapter ma;
-
   static Toolkit toolkit;
 
-	public void paint(Graphics g) 
+  public MainWindow(){
+	addMouseListener(new MouseAdapter() { 
+		public void mousePressed(MouseEvent me) { 
+		 System.out.println(me); 
+		} 
+	}); 
+  }
+
+	public void paintInit(Graphics g) 
 	{  
 		if(toolkit == null)
 		{
 			toolkit = Toolkit.getDefaultToolkit();
 		}
-		
-  }  
+	}  
 
 	public static void main(String[] args)
 	{
@@ -34,26 +38,22 @@ public class MainWindow extends Canvas
 		pixelLocationsY = new int[8];
 
 		MainWindow m=new MainWindow();  
-    JFrame f=new JFrame();  
-    f.add(m);  
-    f.setSize(250,250);  
-    f.setVisible(true); 
+    	JFrame f=new JFrame();  
+    	f.add(m);  
+    	f.setSize(1000,1000);  
+    	f.setVisible(true); 
 
-    Point cursorPoint = MouseInfo.getPointerInfo().getLocation();
-    
+		if(toolkit == null)
+		{
+			toolkit = Toolkit.getDefaultToolkit();
+		}
+
+		
 	}
 
-  private static void cursorLocation(Point cursorPoint)
-  {
-    cursorPoint = MouseInfo.getPointerInfo().getLocation();
-    cursorX = cursorPoint.x;
-    cursorY = cursorPoint.y;
-    //Note: blue bar is part of window. bottom of blue bar, y=25. top of frame, y=0
-  }
-
-	public static void onClick(MouseEvent e)
-	{
-		//System.out.println(cursorPoint);
-	}
+    private static Point cursorLocation()
+    {
+      return MouseInfo.getPointerInfo().getLocation();
+    }
 
 }
